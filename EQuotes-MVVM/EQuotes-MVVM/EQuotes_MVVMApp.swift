f5aa820952569abcd93be72fbe9b3e3d77cbe74e
Thuyen Truong <thuyentruong.work@gmail.com>
2023-04-20 19:56:37 +0700

    [c] config Firebase

--- EQuotes-MVVM/EQuotes-MVVM/EQuotes_MVVMApp.swift
+++ EQuotes-MVVM/EQuotes-MVVM/EQuotes_MVVMApp.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct EQuotes_MVVMApp: App {
+
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+
     }
 }
