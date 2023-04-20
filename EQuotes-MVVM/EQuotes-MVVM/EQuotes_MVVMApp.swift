ef59d55b81e6e97af703b1277b5d7fc9bb27d2b8
Thuyen Truong <thuyentruong.work@gmail.com>
2023-04-20 19:37:07 +0700

    init project

---
+++ EQuotes-MVVM/EQuotes-MVVM/EQuotes_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  EQuotes_MVVMApp.swift
+//  EQuotes-MVVM
+//
+//  Created by Thuyên Trương on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct EQuotes_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
